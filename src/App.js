773c5ca125d0a635f033d98db75789427ff2cd4e
Dominique Gibson <DGibson8822@gmail.com>
2024-03-05 12:39:22 -0600

    Added api call to retrieve characters from database. Added state to keep track of these characters client side.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import './App.css';
 
 // components
@@ -6,9 +6,29 @@ import CharacterCard from './components/CharacterCard';
 
 
 function App() {
+  const [characters, setCharacters] = useState([])
+
+  const getCharacters = async() => {
+    try {
+      const response = await fetch('http://localhost:5000/characters')
+      const charactersJSON = await response.json()
+      setCharacters(charactersJSON);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+  
+  useEffect( () => {
+    getCharacters();
+  }, [])
+  
+  console.log(characters)
   return (
     <Fragment>
       <div className="container">
+        {characters.map( character => (
+          <CharacterCard characterData={character}/>
+        ))}
         
       </div>
     </Fragment>
